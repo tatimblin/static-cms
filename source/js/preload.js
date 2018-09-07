233138d8f77080787e30bc267d4c63d4d71a5d40
Tristan Timblin <tatimblin@gmail.com>
2018-09-07 10:46:50 -0400

    Preload images from slider

---
+++ source/js/preload.js
@@ -0,0 +1,38 @@
+function preloadImages(srcs) {
+    function loadImage(src) {
+        return new Promise(function(resolve, reject) {
+            var img = new Image();
+            img.onload = function() {
+                resolve(img);
+            };
+            img.onerror = img.onabort = function() {
+                reject(src);
+            };
+            img.src = src;
+        });
+    }
+    var promises = [];
+    for (var i = 0; i < srcs.length; i++) {
+        promises.push(loadImage(srcs[i]));
+    }
+    return Promise.all(promises);
+}
+
+preloadImages(
+    [
+        // Hero Slider
+        'images/assets/otto-hero-commute.jpg', 
+        'images/assets/otto-hero-nature.jpg', 
+        'images/assets/otto-hero-community.jpg',
+        // Floorplan
+        // Story Flicker
+        'images/assets/otto-story-ext.jpg',
+        'images/assets/otto-story-hist-ext.jpg',
+        'images/assets/otto-story-hist-int.jpg'
+        // Brewerytown Slider
+        // Plan Slider
+    ]).then(function(imgs) {
+    console.log('images loaded');
+}, function(errImg) {
+    console.log('error loading images');
+});
